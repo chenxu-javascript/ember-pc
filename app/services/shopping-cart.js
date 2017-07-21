b9e8ec357e02ed741f6095844d7ff0253ab452ae
843313654@qq.com <chenxu@zbj.com>
2017-07-21 18:40:56 +0800

    dist

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -8,8 +8,28 @@ export default Ember.Service.extend({
     this.set('items', []);
   },
 
+  getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = '-';
+    var seperator2 = ':';
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    var currentdate = date.getFullYear() + seperator1 + this.addzero(month) + seperator1 + this.addzero(strDate) +
+      ' ' + this.addzero(date.getHours()) + seperator2 + this.addzero(date.getMinutes()) +
+      seperator2 + this.addzero(date.getSeconds());
+    return currentdate;
+  },
+
+  addzero(num) {
+    if (num >= 1 && num <= 9) {
+      num = '0' + num;
+    }
+    return num;
+  },
+
   add(item) {
     this.get('items').pushObject(item);
+    localStorage.setItem('count', this.getNowFormatDate());
   },
 
   remove(item) {

db1ea7949eece860e1296696200fdeacc0805189
843313654@qq.com <chenxu@zbj.com>
2017-07-22 11:21:42 +0800

    “删除精简版

--- app/router.js
+++ app/router.js
@@ -11,14 +11,7 @@ Router.map(function() {
   this.route('powerselect');
   this.route('injectsevers');
   this.route('select');
-  this.route('liquidfire');
-  this.route('ember');
   this.route('team');
-  this.route('img');
-  this.route('backgammon');
-  this.route('emberfn', { path: '/emberfn' }, function() {
-    this.route('embera', { path: '/embera' });
-  });
 });
 
 export default Router;

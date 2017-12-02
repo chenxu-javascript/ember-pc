2533a1c1d8296261efb2d946343969883b60f21a
chenxu <chenxu@zbj.com>
2017-12-02 16:31:11 +0800

    dist

--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -2,4 +2,15 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   // body
+  actions: {
+    postmessage() {
+      let params = {
+        userId: 18,
+        orderId: 188,
+        totalAmount: 1888,
+        productId: 187,
+      };
+      parent.postMessage(JSON.stringify(params), '*');
+    }
+  }
 });
